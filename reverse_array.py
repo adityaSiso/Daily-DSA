1c1c20a7ddca97fbc36e7632e9485d5ca948c77e
Aditya Sisodiya <aditya@voereir.com>
2024-06-24 00:28:57 +0530

    Reverse an Array.

--- reverse_array.py
+++ reverse_array.py
@@ -7,3 +7,12 @@ for i in range(n//2):
     given[i], given[n - i - 1] = given[n - i - 1], given[i]
 
 print(given)
+
+"""
+5, 3, 8, 1, 0 -> 0, 1, 8, 3, 5
+
+Array: 5, 3, 8, 1, 0
+Index: 0, 1, 2, 3, 4 -> 0 <-> 4, 1 <-> 3, <2>
+
+swap index i and n-i.
+"""
