217d4ba5c672ce9d3c06ed084598c1b0c937f2e5
Aditya Sisodiya <aditya@voereir.com>
2024-06-24 23:51:25 +0530

    1. Min and Max in an array. 2. Kth Min and Max Element.

--- Kth_min_and_max_element.py
+++ Kth_min_and_max_element.py
@@ -7,3 +7,5 @@ elements.sort() #Time complexity is nlog(n)
 print(f'{Kth} max element is {elements[-3]}')
 print(f'{Kth} min element is {elements[2]}')
 
+# Solution 1: Just sort and fetch the Kth[Kth - 1 in python] index element.
+# Solution 2: :-/
--- min_and_max_in_array.py
+++ min_and_max_in_array.py
@@ -2,11 +2,11 @@
 
 given = [4, 2, 10, 3, 62, 8, 31, -10]
 
-mx = -1
-mn = 100
+mx = -100 # -ve inf
+mn = 100  # +ve inf
 
 for i in given:
-    mx = max(mx, i)
-    mn = min(mn, i)
+    mx = max(mx, i) # if current max < Ith element update current max.
+    mn = min(mn, i) # if current min > Ith element update current min.
 
 print(f"Max: {mx}, Min: {mn}")
